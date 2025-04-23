464ec38513f0956c97b5a5389f8a10614e8b6a41
a.chaturov <chaturovai@gmail.com>
2025-04-23 22:41:27 +0300

    unit 15.4

--- Module_15/Program.cs
+++ Module_15/Program.cs
@@ -6,111 +6,170 @@ class Program
     {
         var cars = new List<Car>()
         {
-            new Car("Suzuki", "JP"),
-            new Car("Toyota", "JP"),
-            new Car("Opel", "DE"),
-            new Car("Kamaz", "RUS"),
-            new Car("Lada", "RUS"),
-            new Car("Honda", "JP"),
+            new Car() { Model = "SX4", Manufacturer = "Suzuki" },
+            new Car() { Model = "Grand Vitara", Manufacturer = "Suzuki" },
+            new Car() { Model = "Jimny", Manufacturer = "Suzuki" },
+            new Car() { Model = "Land Cruiser Prado", Manufacturer = "Toyota" },
+            new Car() { Model = "Camry", Manufacturer = "Toyota" },
+            new Car() { Model = "Polo", Manufacturer = "Volkswagen" },
+            new Car() { Model = "Passat", Manufacturer = "Volkswagen" },
         };
 
-        var carsByCountry = from car in cars
-            group car by car.CountryCode;
-
-        carsByCountry = cars.GroupBy(car => car.CountryCode);
-
-        foreach (var group in carsByCountry)
+        var manufacturers = new List<Manufacturer>()
         {
-            Console.WriteLine(group.Key + ": ");
+            new Manufacturer() { Country = "Japan", Name = "Suzuki" },
+            new Manufacturer() { Country = "Japan", Name = "Toyota" },
+            new Manufacturer() { Country = "Germany", Name = "Volkswagen" },
+        };
 
-            foreach (var car in group)
-            {
-                Console.WriteLine(car.Manufacturer);
-            }
-
-            Console.WriteLine();
-        }
-
-        var carsByCountry2 = from car in cars
-            group car by car.CountryCode
-            into grouping
+        var result = from car in cars
+            join m in manufacturers on car.Manufacturer equals m.Name
             select new
             {
-                Name = grouping.Key,
-                Count = grouping.Count(),
-                Cars = from p in grouping select p
+                Name = car.Model,
+                Manufacturer = m.Name,
+                Country = m.Country
             };
 
-        carsByCountry2 = cars
-            .GroupBy(car => car.CountryCode)
-            .Select(g => new
+        foreach (var item in result)
+            Console.WriteLine($"{item.Name} - {item.Manufacturer} ({item.Country})");
+
+        Console.WriteLine();
+
+        var result2 = cars
+            .Join(
+                manufacturers,
+                car => car.Manufacturer,
+                m => m.Name
+                , (car, m) => new
+                {
+                    Name = car.Model,
+                    Manufacturer = m.Name,
+                    Country = m.Country
+                });
+
+        foreach (var item in result2)
+            Console.WriteLine($"{item.Name} - {item.Manufacturer} ({item.Country})");
+
+        Console.WriteLine();
+
+        var result3 = manufacturers.GroupJoin(cars,
+            m => m.Name,
+            car => car.Manufacturer,
+            (m, crs) => new
             {
-                Name = g.Key,
-                Count = g.Count(),
-                Cars = g.Select(c => c)
+                Name = m.Name,
+                Country = m.Country,
+                Cars = crs.Select(c => c.Model)
             });
 
-        foreach (var group in carsByCountry2)
+        foreach (var item in result3)
         {
-            Console.WriteLine($"{group.Name}: {group.Count}");
+            Console.WriteLine($"{item.Name} ({item.Country}):");
 
-            foreach (var car in group.Cars)
-                Console.WriteLine(car.Manufacturer);
+            foreach (var model in item.Cars)
+                Console.WriteLine($"{model}");
 
             Console.WriteLine();
         }
 
+        Console.WriteLine();
 
-        var phoneBook = new List<Contact>();
-
-        phoneBook.Add(new Contact("Игорь", 79990000001, "igor@example.com"));
-        phoneBook.Add(new Contact("Сергей", 79990000010, "serge@example.com"));
-        phoneBook.Add(new Contact("Анатолий", 79990000011, "anatoly@example.com"));
-        phoneBook.Add(new Contact("Валерий", 79990000012, "valera@example.com"));
-        phoneBook.Add(new Contact("Сергей", 799900000013, "serg@gmail.com"));
-        phoneBook.Add(new Contact("Иннокентий", 799900000013, "innokentii@example.com"));
-
-        var grouped = phoneBook.GroupBy(p => p.Email.Split('@').Last());
-        foreach (var group in grouped)
+        var departments = new List<Department>()
         {
-            if (group.Key.Contains("example"))
-            {
-                Console.WriteLine("Фейковые адреса:");
-                foreach (var contact in group)
-                    Console.WriteLine($"{contact.Name} {contact.PhoneNumber} {contact.Email}");
-            }
-            else
-            {
-                Console.WriteLine("Реальные адреса:");
-                foreach (var contact in group)
-                    Console.WriteLine($"{contact.Name} {contact.PhoneNumber} {contact.Email}");
-            }
-        }
-    }
+            new() { Id = 1, Name = "Программирование" },
+            new() { Id = 2, Name = "Продажи" }
+        };
 
-    class Contact
-    {
-        public string Name { get; set; }
-        public long PhoneNumber { get; set; }
-        public string Email { get; set; }
-
-        public Contact(string name, long phoneNumber, string email)
+        var employees = new List<Employee>()
         {
-            Name = name;
-            PhoneNumber = phoneNumber;
-            Email = email;
+            new() { DepartmentId = 1, Name = "Инна", Id = 1 },
+            new() { DepartmentId = 1, Name = "Андрей", Id = 2 },
+            new() { DepartmentId = 2, Name = "Виктор ", Id = 3 },
+            new() { DepartmentId = 3, Name = "Альберт ", Id = 4 },
+        };
+
+        var resultWorkers = from employee in employees
+            join d in departments on employee.DepartmentId equals d.Id
+            select new
+            {
+                Name = employee.Name,
+                Department = d.Name
+            };
+
+        foreach (var item in resultWorkers)
+            Console.WriteLine($"{item.Name}, отдел {item.Department}");
+
+        Console.WriteLine();
+
+        var resultWorkers2 = employees
+            .Join(departments,
+                employee => employee.DepartmentId,
+                d => d.Id,
+                (employee, d) => new
+                {
+                    Name = employee.Name,
+                    Department = d.Name
+                });
+
+        foreach (var item in resultWorkers2)
+            Console.WriteLine($"{item.Name}, отдел {item.Department}");
+
+        Console.WriteLine();
+
+        var resultWorker3 = departments.GroupJoin(
+            employees,
+            d => d.Id,
+            emp => emp.DepartmentId,
+            (d, emps) => new
+            {
+                Department = d.Name,
+                Employees = emps.Select(e => e.Name)
+            });
+
+        foreach (var item in resultWorker3)
+        {
+            Console.WriteLine($"Отдел \"{item.Department}\":");
+
+            foreach (var employee in item.Employees)
+                Console.WriteLine($"Сотрудник {employee}");
+
+            Console.WriteLine();
         }
+
+        Console.WriteLine();
+
+        var letters = new string[] { "A", "B", "C", "D", "E" };
+        var numbers = new int[] { 1, 2, 3 };
+
+        var res = letters.Zip(numbers, (l, n) => $"{l}{n}");
+
+        foreach (var item in res)
+            Console.WriteLine(item);
     }
 
     public class Car
     {
+        public string Model { get; set; }
         public string Manufacturer { get; set; }
-        public string CountryCode { get; set; }
+    }
 
-        public Car(string brand, string country)
-        {
-            Manufacturer = brand;
-            CountryCode = country;
-        }
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
